97cf8882bc9496f069c8d1d241e35d0d0a11910b
Luiz Eduardo de Oliveira Fonseca <luizeof@gmail.com>
2026-03-29 22:44:29 -0300

    chore: add Cloudflare adapter and create wrangler configuration file

--- astro.config.js
+++ astro.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ctrimm.github.io',
+  site: 'https://setupvibe.dev',
   base: '/',
+  output: 'static',
+  adapter: cloudflare(),
   integrations: [
     react(),
     sitemap(),
