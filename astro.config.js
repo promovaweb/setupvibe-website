20eaa456cfd66c2b50055a8bf7c3b171c9c9b8e9
Luiz Eduardo de Oliveira Fonseca <luizeof@gmail.com>
2026-03-30 00:54:01 -0300

    feat: add @playform/compress integration and update dependencies
    - Integrated @playform/compress into astro.config.js
    - Updated package-lock.json and package.json to include @playform/compress version 0.2.2

--- astro.config.js
+++ astro.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import Compress from '@playform/compress';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
   integrations: [
     react(),
     sitemap(),
-    mdx()
+    mdx(),
+    Compress()
   ]
 });
