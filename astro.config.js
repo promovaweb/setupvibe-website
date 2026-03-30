31a6e5b57c0ac6601a18e1476d77f8f3062cd6be
Luiz Eduardo de Oliveira Fonseca <luizeof@gmail.com>
2026-03-29 22:57:24 -0300

    chore: remove Cloudflare adapter from Astro configuration

--- astro.config.js
+++ astro.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://setupvibe.dev',
   base: '/',
   output: 'static',
-  adapter: cloudflare(),
   integrations: [
     react(),
     sitemap(),
